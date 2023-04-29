af25a60b3e0e3c5523416ce8ab91684703226680
James Lyerla <lyerlajd@gmail.com>
2023-04-29 00:45:02 -0500

    testing some layout things

---
+++ frontend/src/app/Orders/app.order-component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:'app-order',
+  templateUrl: './app.order-component.html'
+})
+
+export class OrderComponent {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/app.home-component';
+import { OrderComponent } from './Orders/app.order-component';
+import { ReviewComponent } from './Reviews/app.review-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrderComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
