0846c9d72fd80c1d9ce5da9f03f05a626de06420
jstepanek <55816220+jstepanek@users.noreply.github.com>
2023-05-03 23:52:31 -0500

    fetching menu from server using httpClient GET api

--- frontend/src/app/Orders/app.order-component.ts
+++ frontend/src/app/Orders/app.order-component.ts
@@ -1,8 +1,53 @@
 import { Component } from "@angular/core";
+import { HttpClient} from "@angular/common/http";
+import { async } from "@angular/core/testing";
+import axios from "axios";
 
 @Component({
   selector:'app-order',
   templateUrl: './app.order-component.html'
 })
 
-export class OrderComponent {}
+export class OrderComponent {
+  
+  httpClient: HttpClient;
+  message: string;
+
+  /*menuItems will contain:
+
+    menuItem._id
+    menuItem.name
+    menuItem.price
+    menuItem.description
+    menuItem.options: 
+    [{
+			style: { type: String },
+			price: { type: String },
+		}],
+    menuItem.type
+    menuItem.category
+    
+  */
+  menuItems: any[];
+
+  constructor(public http: HttpClient) {
+    this.httpClient = http;
+    this.message = "";
+    this.menuItems = [];
+  }
+
+  ngOnInit(){
+    this.fetchMenu();
+  }
+
+  fetchMenu()
+  {
+      this.httpClient.get<{message: string, data: any}>('http://localhost:5001/api/menu/get-menu')
+      .subscribe(data => {
+        this.message = data.message;
+        this.menuItems = data.data;
+      })   
+      return;
+  }
+
+}
