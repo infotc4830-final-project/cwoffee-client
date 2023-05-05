4e5e38f91803f629cc39523959c44a0703c13a16
jstepanek <55816220+jstepanek@users.noreply.github.com>
2023-05-04 19:56:40 -0500

    order/review updates, basic login/register

--- frontend/src/app/Login/login.component.ts
+++ frontend/src/app/Login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppService } from '../app.service';
+import { UserInfo } from './userInfo';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string;
+  password: string;
 
+  constructor(public appService: AppService){
+    this.username = "";
+    this.password = "";
+  }
+  
+  handleRegister(){
+    const userInfo: UserInfo = {username: this.username, password: this.password};
+    this.appService.postRegister(userInfo).subscribe(response => { console.log(response); });
+  }
+
+  handleLogin(){
+    const userInfo: UserInfo = {username: this.username, password: this.password};
+    this.appService.postLogin(userInfo).subscribe(response => { console.log(response); });
+  }
 }
