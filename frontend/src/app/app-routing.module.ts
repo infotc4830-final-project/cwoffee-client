63d27624f5d8707a2860d9732e146f50755483a5
jstepanek <55816220+jstepanek@users.noreply.github.com>
2023-05-04 20:34:30 -0500

    routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './Home/app.home-component';
+import { LoginComponent } from './Login/login.component';
+import { OrderComponent } from './Orders/app.order-component';
+import { ReviewComponent } from './Reviews/app.review-component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'login', component: LoginComponent
+  }];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
